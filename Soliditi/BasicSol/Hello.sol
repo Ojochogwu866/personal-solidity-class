7c41cca0e413b639df8f1ce4a34c5c4a09c247ec
Ojochogwu <07afrik@gmail.com>
2023-01-14 10:30:39 +0100

    fix-errors

--- Soliditi/BasicSol/Hello.sol
+++ Soliditi/BasicSol/Hello.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
 
 contract Greeter{
     string private _greeting;
---
+++ Soliditi/Sol-2/DataStructure.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract dataTypes{
+     /**
+       VARIABLES
+     Variables are of two types.
+     Local and State Variables
+     * State variables are stored and exist on the blockchain.
+     *Local variables are only accessible to the function. The variable scope is within the function
+     */
+
+    //stateVariables  
+    int public myInt = 1;
+    uint public myUint = 1;
+    uint256 public myUint256 = 1; 
+    uint8 public myUint8 = 1;
+    string public myString = "Hello, Netherlands";    //strings: they are data types can also be stored in a smart contract
+    bytes32 public myBytes32 = "Hello Netherlands";  //bytes32
+    address public amyAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;  //address: Every blockchain has an address
+
+
+
+    //local variable
+    function getuintValue() public pure returns(uint){
+        uint value = 1;
+        return value;
+    }
+   //local variable
+    function getValue() public pure returns(string){  
+        return value;
+    }
+    //local variable
+     function getValue() public pure returns(uint){
+        bytes32 value = "Hello, Netherlands";
+        return value;
+    }
+     //localVariable
+    function dataType() public pure returns (uint){
+        uint value = 1; //unassigned integer
+        return value;
+    }
+
+    //creating custom Data structures
+    struct MyStruct {
+        uint myUint;
+        string myString;
+    }
+     MyStruct public myStruct = MyStruct(1, "Hello, Netherlands!");
+
+}
