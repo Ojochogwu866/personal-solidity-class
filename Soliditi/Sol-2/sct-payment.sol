c1436da2ab7722e21e6f09f7d0f07b18d595e11f
Ojochogwu <07afrik@gmail.com>
2023-01-16 21:14:55 +0100

    -smct

--- Soliditi/Sol-2/sct-payment.sol
+++ Soliditi/Sol-2/sct-payment.sol
@@ -2,27 +2,29 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract payUber{
-    address payable public owner;
     enum Statuses { Free, Booked }
     Statuses currentStatus;
-    event Booked(address _user, uint _value);
+    address payable public owner;
+    
+    event Book(address _passenger, uint _value);
+
     constructor() public{
-        owner = msg.sender;
+        owner = payable(msg.sender);
         currentStatus = Statuses.Free; 
     }
 
-    modifier onlyWhileFree{
-        require(currentStatus == Statuses.Free, "Currently in service");
+    modifier onlyWhileFree {
+        require(currentStatus == Statuses.Free, "Currently occupied.");
         _;
     }
-    modifier costs (uint _amount){
-         require(msg.value >= _amount, "Not Enough Ether provided");
-         _;
+    modifier costs(uint _amount) {
+        require(msg.value >= _amount, "Not enough Ether provided.");
+        _;
     }
 
-   recieve() external payable onlyWhileFree costs(2 ether){
-        currentStatus = Statuses.Free;
-        owner.tranfer(msg.value);
-        emit Booked(msg.sender, msg.value);
+    receive() external payable onlyWhileFree costs(2 ether) {
+        currentStatus = Statuses.Booked;
+        owner.transfer(msg.value);
+        emit Book(msg.sender, msg.value);
     }
 }
